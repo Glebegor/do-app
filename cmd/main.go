0de7d5cdefef6d4113f3c71b426087e8209c8be9
unknown <glebegor2220@gmail.com>
2023-03-02 12:25:50 +0100

    Created db Postgres

--- cmd/main.go
+++ cmd/main.go
@@ -7,14 +7,42 @@ import (
 
 	todo "github.com/Glebegor/do-app"
 	handler "github.com/Glebegor/do-app/pkg/handler"
+	repository "github.com/Glebegor/do-app/pkg/repository"
+	service "github.com/Glebegor/do-app/pkg/service"
+	_ "github.com/lib/pq"
+	viper "github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing config: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDb(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "vlgleg555",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("failed to initializate db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	server := new(todo.Server)
 	fmt.Printf("%s: Server is running on port 8000. BTW", time.Now())
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error while running server: %s, %s", err.Error(), time.Now())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,10 +1,17 @@
 package handler
 
 import (
+	"github.com/Glebegor/do-app/pkg/service"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
+	services *service.Service
+	// repositoryes *repository.Repository
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
